58394739bd0f6b9bb9ab0ce493970710a3ea7206
jaucoin1990 <jaucoin@cbtnuggets.com>
2020-08-10 17:55:42 -0700

    updates file

--- definition-script/def-update.py
+++ definition-script/def-update.py
@@ -12,21 +12,27 @@ def value_returner(key, file=data):
     return file[key]
 
 
-print("This is a dictionary. Enter the word you'd like defined. When you're done, type 'end.'")
+print("This is a dictionary. Enter the word you'd like defined. When you're done using me to define words, type 'end' instead of a word.")
 while True:
-    key = input("Enter word: ").lower()
+    key = input("\nEnter word: ").lower()
 
     if key == "end":
         break
     elif key in data:
-        print(value_returner(key))
+        list = value_returner(key)
+        print("\nYour definition is:\n")
+        for i in list:
+            print("\n{}\n".format(i))
     elif similar(key):
        list = similar(key)
        for i in list:
-           print("Did you mean {}?".format(i))
-           answer = input("Yes or no? ").lower()
-           if answer == "yes":
-               print(value_returner(i))
+           print("\nDid you mean {}?".format(i))
+           answer = input("\nYes or no? ").lower()
+           if answer == "yes" or answer == "y":
+               new_list = value_returner(i)
+               print("\nYour definition is:\n")
+               for i in new_list:
+                   print("\n{}\n".format(i))
                break
     else:
         print("I don't know that word. Try a different word.")
